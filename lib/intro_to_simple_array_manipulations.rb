a0b06af70dc89ad2ac9267b1c1e1208168786a22
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-07 19:24:17 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def array_push_method(some_array, "mint")
+  some_array = ["cucumber", "lemon"]
+  new_ingredient = "mint"
+  some_array.push(new_ingredient)
+end 
+
+def array_unshift_method(smoothie_array, "rose")
+  smoothie_array = ["plain yogurt", "rose water", "sugar"]
+  rose_ingredient = "rose"
+  smoothie_array.unshift("rose")
+end 
+
+def array_pop_method
+  array_pop_method = ["array", "pop", "method"]
+  array_pop_method.pop
+end 
